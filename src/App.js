84875efe84b0d5e4d64fe17ed251f3e3a812c1a0
Natanael <natanael.santana@outlook.com.br>
2023-06-06 08:35:42 -0300

    tailwind added, basic components stylized

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import React from "react";
+//import { BrowserRouter, Route } from "react-router-dom";
+
+//Principal components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Products from "./components/ProductList";
+
+
+
+const App = () => {
+  return (
+
+    <div>
+      <Header />
+      <Products />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
