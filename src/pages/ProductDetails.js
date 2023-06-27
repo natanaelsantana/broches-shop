b9c0fbe75a0a226a03034e3a7c7d3253f69cade9
Natanael <natanael.santana@outlook.com.br>
2023-06-27 16:06:40 -0300

    Some changes in the link

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-
-import 'dotenv/config'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
-
 import axios from 'axios';
 
-const ProductDetails = ({ match }) => {
+
+const URI = process.env.URL;
+
+const ProductDetails = () => {
+  const { idUnico } = useParams();
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await axios.get(`${process.env.URI}/produtos/${match.params.id}`);
+        const response = await axios.get(`${URI}/produtos/${idUnico}`);
         setProduct(response.data);
       } catch (error) {
         console.error(error);
       }
     };
-
     fetchProduct();
-  }, [match.params.id]);
+  }, [idUnico]);
 
   if (!product) {
     return <div>Loading...</div>;
@@ -30,7 +30,7 @@ const ProductDetails = ({ match }) => {
 
   return (
     <div>
-    <Header />
+      <Header />
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p>Price: R${product.price.toFixed(2)}</p>
@@ -46,4 +46,3 @@ const ProductDetails = ({ match }) => {
 };
 
 export default ProductDetails;
-
