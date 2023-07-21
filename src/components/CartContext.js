7202e0e29e0e863cf420ec1d85f17b8fd2b0e78e
Natanael <natanael.santana@outlook.com.br>
2023-07-21 09:07:54 -0300

    Component to add items to shopping cart

---
+++ src/components/CartContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+
+export const useCartContext = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartItems((prevItems) => [...prevItems, product]);
+  };
+
+  const removeFromCart = (productId) => {
+    setCartItems((prevItems) =>
+      prevItems.filter((item) => item.id !== productId),
+    );
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cartItems, addToCart, removeFromCart, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
