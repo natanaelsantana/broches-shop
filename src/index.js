3359ff0003035d80735ae0ce4f17e682d44ad62b
Natanael <natanael.santana@outlook.com.br>
2023-08-01 07:06:19 -0300

    backend  start

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'tailwindcss/tailwind.css';
-import store from './config/store';
+import store from './redux/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
