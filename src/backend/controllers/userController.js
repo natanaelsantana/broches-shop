21e1349da00299c239bd3194d741e12050a536c4
Natanael <natanael.santana@outlook.com.br>
2023-08-05 13:04:26 -0300

    Adjust in backend routes. Controllers added

---
+++ src/backend/controllers/userController.js
@@ -0,0 +1,82 @@
+require('dotenv').config();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const generateToken = require('../utils/generateToken');
+const saltRounds = 12;
+
+const Register = async (req, res) => {
+  try {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Max-Age', '1800');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'PUT, POST, GET, DELETE, PATCH, OPTIONS',
+    );
+
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: 'Usuário já cadastrado' });
+    }
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'Usuário cadastrado com sucesso' });
+  } catch (err) {
+    res.status(500).json({ message: 'Falha no cadastro' });
+  }
+};
+
+const Login = async (req, res) => {
+  try {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Max-Age', '1800');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'PUT, POST, GET, DELETE, PATCH, OPTIONS',
+    );
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: 'Credenciais inválidas' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: 'Senha inválida' });
+    }
+
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } catch (err) {
+    res.status(500).json({ error: 'Falha no login' });
+  }
+};
+
+const Logout = async (req, res) => {
+  try {
+    res.cookie('jwt', '', {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    res.status(200).json({ message: 'logged out' });
+  } catch {
+    res.status(500).json({ error: 'Falha no logout' });
+  }
+};
+
+module.exports = { Register, Login, Logout };
--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -1,86 +1,12 @@
+const controller = require('../controllers/userController');
 const express = require('express');
-require('dotenv').config();
-const User = require('../models/user');
-
-const bcrypt = require('bcrypt');
-const generateToken = require('../utils/generateToken');
-const saltRounds = 12;
 
 const router = express.Router();
 
-router.post('/cadastro', async (req, res) => {
-  try {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Max-Age', '1800');
-    res.setHeader('Access-Control-Allow-Headers', 'content-type');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'PUT, POST, GET, DELETE, PATCH, OPTIONS',
-    );
+router.post('/register', controller.Register);
 
-    const { name, email, password } = req.body;
+router.post('/login', controller.Login);
 
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ error: 'Usuário já cadastrado' });
-    }
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const newUser = new User({ name, email, password: hashedPassword });
-    await newUser.save();
-
-    res.status(201).json({ message: 'Usuário cadastrado com sucesso' });
-  } catch (err) {
-    res.status(500).json({ message: 'Falha no cadastro' });
-  }
-});
-
-router.post('/login', async (req, res) => {
-  try {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Max-Age', '1800');
-    res.setHeader('Access-Control-Allow-Headers', 'content-type');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'PUT, POST, GET, DELETE, PATCH, OPTIONS',
-    );
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ error: 'Credenciais inválidas' });
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(400).json({ error: 'Senha inválida' });
-    }
-
-    generateToken(res, user._id);
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-    });
-  } catch (err) {
-    res.status(500).json({ error: 'Falha no login' });
-  }
-});
-
-router.post('/logout', async (req, res) => {
-  try {
-    res.cookie('jwt', '', {
-      httpOnly: true,
-      expires: new Date(0),
-    });
-
-    res.status(200).json({ message: 'logged out' });
-  } catch {
-    res.status(500).json({ error: 'Falha no logout' });
-  }
-});
+router.post('/logout', controller.Logout);
 
 module.exports = router;
